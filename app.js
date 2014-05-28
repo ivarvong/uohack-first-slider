4a5b6550041d9e50bb59777862f7fa838e1edfe6
Ivar Vong <ivar@ivarvong.com>
2014-05-27 19:22:46 -0700

    resize slides with js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	console.log('hello!');
+	var width = $(window).width();
+	var height = $(window).height();
+	var $slides = $('.slides li');
+	$slides.css('width',  (width - 80 - 100)+'px');
+	$slides.css('height', (height - 80 - 100)+'px');
+
+	$slides.each(function(index, slide) {
+		$(slide).css('left', index*width+'px')
+	});
 });
